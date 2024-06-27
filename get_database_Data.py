34b4b736cb73e07bfaf360a0318d71a86f9834e4
ariel055132 <ariel055132@gmail.com>
2024-06-27 08:02:56 +0800

    Load data from chroma (vector database)

---
+++ get_database_Data.py
@@ -0,0 +1,27 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+# a function for fetching embeddings in chroma db
+def fetch_embeddings():
+    collection = Chroma(persist_directory=CHROMA_PATH, embedding_function=OpenAIEmbeddings())
+    print(collection.get().keys())
+    embeddings = collection.get(include=["embeddings"])
+    return embeddings
+
+# Load data from vector database.
+
+CHROMA_PATH = "chroma"
+
+load_dotenv('config.env')
+print(os.environ.get('OPENAI_API_KEY'))
+
+vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=OpenAIEmbeddings())
+embeddings = fetch_embeddings()
+#print(embeddings)
+# {'ids': [], 'embeddings': None, 'documents': [], 'metadatas': []}
+#print(vectordb.get())
+
+
+
