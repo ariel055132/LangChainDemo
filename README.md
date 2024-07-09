2ad3652888f6cceeb378a5b82158ded0418951ea
ariel055132 <ariel055132@gmail.com>
2024-07-09 23:31:53 +0800

    update readme and constants, add the service operation csv file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LangChainDemo
+* A LangChainDemo with RAG to implement drawing sequence diagram of different BIAN process.
--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 CHROMA_BIAN_PATH = "chroma_bian"
+CHROMA_BIAN_SD_SO_PATH = "chroma_bian_sd_so"
 CHROMA_PATH = "chroma"
 
 DATA_PATH = "data/books"
